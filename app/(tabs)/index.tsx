d2791feaa7ac340bc6fc423324b11c1dd5732cb4
phea <sokleapheaphon@gmail.com>
2025-09-18 16:55:26 +0700

    first commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+ 
+export default function App() {
+  return (
+    // <View className="flex-1 items-center justify-center bg-white">
+      <Text style={{color: "white"}}>
+     
+        Welcome to Nativewind!
+      </Text>
+    // </View>
+  );
+}
